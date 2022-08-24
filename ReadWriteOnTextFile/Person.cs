fee3d5797286c0ab2cede4363ec6b4578ab6bd15
Nikolay Totuhov <nikolaytotuhov@gmail.com>
2022-08-24 20:37:32 +0200

    1.1

---
+++ ReadWriteOnTextFile/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadWriteOnTextFile
+{
+    internal class Person
+    {
+        public Person(string firstName, string lastName, string birthDate)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.BirthDate = birthDate;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+    }
+}
---
+++ ReadWriteOnTextFile/Program.cs
@@ -0,0 +1,41 @@
+﻿using ReadWriteOnTextFile;
+
+List<Person> people = new List<Person>();
+
+string filePath = @"C:\Users\User\Desktop\CSharpTraining\ReadWriteOnTextFile\data.txt";
+
+List<string> lines = File.ReadAllLines(filePath).ToList();
+
+foreach (string line in lines)
+{
+    string[] input = line.Split(",");
+
+    string firstName = input[0];
+    string lastName = input[1];
+    string birthDate = input[2];
+
+    Person person = new Person(firstName, lastName, birthDate);
+
+    people.Add(person);
+}
+
+foreach (Person person in people)
+{
+    Console.WriteLine($"{person.FirstName} {person.LastName}: {person.BirthDate}");
+}
+
+    people.Add(new Person("Yordanka", "Gancheva", "22.03.1963"));
+
+
+List<string> output = new List<string>();
+
+foreach (var person in people)
+{
+    output.Add($"{person.FirstName},{person.LastName},{person.BirthDate}");
+}
+
+Console.WriteLine("Writing to text file");
+
+File.WriteAllLines(filePath, output);
+
+
